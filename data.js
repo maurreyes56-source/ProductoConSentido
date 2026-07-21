eded6a79d73dfd13a8e39c64c0a8b189d17f6a7b
maurreyes56-source <maurreyes56@gmail.com>
2026-07-21 10:26:37 -0600

    Actualizar Producto Consentido 2026-07-21 10:26

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.PC_DATA = {
-    "actualizado":  "21/07/2026 10:24",
+    "actualizado":  "21/07/2026 10:26",
     "corteDia":  21,
     "diasRestantes":  10,
     "diasCampanaTranscurridos":  6,
